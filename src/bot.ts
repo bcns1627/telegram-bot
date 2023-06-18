967e8191aecc45a288c05a043595327cdefa8083
bcns1627 <136963834+bcns1627@users.noreply.github.com>
2023-06-18 10:56:10 +0000

    OK NOW CLEANED

--- src/bot.ts
+++ src/bot.ts
@@ -1,20 +1,84 @@
+import { Bot, InlineKeyboard, webhookCallback } from "grammy";
+import { chunk } from "lodash";
+import express from "express";
+
+
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
-// Handle the /xd command to reply with "XDD FUCK YOU"
-bot.command("xd", (ctx: any) => {
-  ctx.reply("XDD FUCK YOU");
-});
+// Handle the /yo command to greet the user
+bot.command("yo", (ctx) => ctx.reply(`Yo n1ger ${ctx.from?.username}`));
 
-// Handle all other commands and messages
-bot.on("message", (ctx: any) => {
-  // Ignore all other commands and messages
-});
+
+
+
+const textEffectResponseAccessor = (
+  originalText: string,
+  modifiedText?: string
+) =>
+  `Original: ${originalText}` +
+  (modifiedText ? `\nModified: ${modifiedText}` : "");
+
+const parseTextEffectResponse = (
+  response: string
+): {
+  originalText: string;
+  modifiedText?: string;
+} => {
+  const originalText = (response.match(/Original: (.*)/) as any)[1];
+  const modifiedTextMatch = response.match(/Modified: (.*)/);
+
+  let modifiedText;
+  if (modifiedTextMatch) modifiedText = modifiedTextMatch[1];
+
+  if (!modifiedTextMatch) return { originalText };
+  else return { originalText, modifiedText };
+};
+
+
+// Handle the /about command
+const aboutUrlKeyboard = new InlineKeyboard().url(
+  "Host your own bot for free.",
+  "https://cyclic.sh/"
+);
+
+// Suggest commands in the menu
+bot.api.setMyCommands([
+  { command: "yo", description: "Be greeted by the bot" },
+  {
+    command: "effect",
+    description: "Apply text effects on the text. (usage: /effect [text])",
+  },
+]);
+
+// Handle all other messages and the /start command
+const introductionMessage = `Hello! I'm a Telegram NIGERIAN bot.
+IM NIGGER WELCOME
+
+<b>Commands</b>
+/XD - GETTING FUCKED
+`
+
+const replyWithIntro = (ctx: any) =>
+  ctx.reply(introductionMessage, {
+    reply_markup: aboutUrlKeyboard,
+    parse_mode: "HTML",
+  });
+
+bot.command("start", replyWithIntro);
+bot.on("message", replyWithIntro);
 
 // Start the server
 if (process.env.NODE_ENV === "production") {
   // Use Webhooks for the production server
-  // ...
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
 } else {
   // Use Long Polling for development
   bot.start();
