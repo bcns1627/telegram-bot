c0e766b9c1643b66dae32e40e37b1eb6dd884040
bcns1627 <136963834+bcns1627@users.noreply.github.com>
2023-06-18 12:36:36 +0000

    only test revert

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Bot, InlineKeyboard, webhookCallback } from "grammy";
+import { Bot, InlineKeyboard } from "grammy";
 import { chunk } from "lodash";
 import express from "express";
 import { lastTxnFunction } from "./lasttxn";
@@ -6,36 +6,30 @@ import { lastTxnFunction } from "./lasttxn";
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
-
 //TRACE ADDRESS FUNC START
-//TRACE ADDRESS FUNC START
-const tracedAddresses = new Set<string>(); // Specify the type for tracedAddresses
+const tracedAddresses = new Set<string>();
 
 bot.command("trace", (ctx) => {
-  const address = ctx.message?.text?.split(" ")[1]; // Extract the address from the command
+  const address = ctx.message?.text?.split(" ")[1];
   if (address) {
-    tracedAddresses.add(address); // Add the traced address to the set
+    tracedAddresses.add(address);
     ctx.reply(`Address traced = ${address}`);
-    // Call the function from /lasttxn.ts with the traced address as an array
-    lastTxnFunction([address]); // Wrap the address in an array
+    lastTxnFunction([address]);
   } else {
     ctx.reply("Please provide an address to trace.");
   }
 });
 
-// Handle the /untrace command to remove the traced address
 bot.command("untrace", (ctx) => {
-  const address = ctx.message?.text?.split(" ")[1]; // Extract the address from the command
+  const address = ctx.message?.text?.split(" ")[1];
   if (address) {
-    tracedAddresses.delete(address); // Remove the traced address from the set
+    tracedAddresses.delete(address);
     ctx.reply(`Address untraced = ${address}`);
-    // Perform any necessary cleanup or actions for removing the traced address
   } else {
     ctx.reply("Please provide an address to untrace.");
   }
 });
 
-// Handle the /traced command to show the traced addresses
 bot.command("traced", (ctx) => {
   if (tracedAddresses.size > 0) {
     const tracedList = Array.from(tracedAddresses).join("\n");
@@ -45,27 +39,10 @@ bot.command("traced", (ctx) => {
   }
 });
 
-// Handle the /untraceall command to remove all traced addresses
 bot.command("untraceall", (ctx) => {
-  tracedAddresses.clear(); // Clear all traced addresses from the set
+  tracedAddresses.clear();
   ctx.reply("All addresses untraced.");
-  // Perform any necessary cleanup or actions for removing all traced addresses
 });
-
-
-
-//TRACED FUNC END
-
-
-// Suggest commands in the menu
-bot.api.setMyCommands([
-  { command: "yo", description: "Be greeted by the bot" },
-  {
-    command: "effect",
-    description: "Apply text effects on the text. (usage: /effect [text])",
-  },
-]);
-
 
 // Handle the /id command to get the group chat ID
 bot.command("id", (ctx) => {
@@ -79,13 +56,20 @@ bot.command("id", (ctx) => {
   }
 });
 
+// Suggest commands in the menu
+bot.api.setMyCommands([
+  { command: "yo", description: "Be greeted by the bot" },
+  {
+    command: "effect",
+    description: "Apply text effects on the text. (usage: /effect [text])",
+  },
+]);
 
 // Start the server
 if (process.env.NODE_ENV === "production") {
   // Use Webhooks for the production server
   const app = express();
   app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
