3bc91bf793f62fce5e31a3ecc98ee88fb9044df5
bcns1627 <136963834+bcns1627@users.noreply.github.com>
2023-06-18 17:07:51 +0000

    revert this bot only start

--- src/bot.ts
+++ src/bot.ts
@@ -79,18 +79,6 @@ bot.command("id", (ctx) => {
   }
 });
 
-// Start the server
-if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
-} else {
-  // Use Long Polling for development
   bot.start();
-}
+
