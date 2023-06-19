336f362b49235f6f84ff697c6dedfa0cbfda6403
bcns1627 <136963834+bcns1627@users.noreply.github.com>
2023-06-19 18:04:18 +0000

    test clear 1

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,21 @@
 import { Bot, InlineKeyboard, webhookCallback } from "grammy";
 import { chunk } from "lodash";
 import express from "express";
-import { lastTxnFunction } from "./lasttxn";
+
 
 // Create a bot using the Telegram token
 export const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
-bot.command("trace", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.command("id", (ctx) => {
+  const chat = ctx.chat;
+   if (chat?.type === "group" || chat?.type === "supergroup") {
+    const groupId = chat.id;
+    const formattedId = `\`${groupId}\``;
+    ctx.reply(`Group Chat ID: ${formattedId}`, { parse_mode: "MarkdownV2" });
+  } else {
+    ctx.reply("This command can only be used in a group chat.");
+  }
+});
 
 
   bot.start();
