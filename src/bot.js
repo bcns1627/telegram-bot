a1958cd665b69e9bd895e71dcca06be10f6ca44c
bcns1627 <136963834+bcns1627@users.noreply.github.com>
2023-06-19 18:59:29 +0000

    NEW CLEAR JS TRY

--- src/bot.js
+++ src/bot.js
@@ -1,52 +1,42 @@
 const { Bot } = require("grammy");
-const { lastTxnFunction } = require("./lasttxn");
+
 
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
-// TRACE ADDRESS FUNC START
-const tracedAddresses = new Set(); // Specify the type for tracedAddresses
 
-bot.command("trace", async (ctx) => {
-  const address = ctx.message?.text?.split(" ")[1]; // Extract the address from the command
-  if (address) {
-    tracedAddresses.add(address); // Add the traced address to the set
-    ctx.reply(`Address traced = ${address}`);
-    // Call the function from /lasttxn.js with the traced address as an array
-    await sendMessageWithRetry(ctx, `Address traced = ${address}`);
-    lastTxnFunction([address]); // Wrap the address in an array
-  } else {
-    ctx.reply("Please provide an address to trace.");
-  }
+// Start command handler
+bot.command("start", (ctx) => {
+  ctx.reply("Welcome to the bot! Type /help for available commands.");
 });
 
-// Helper function to send message with retry
-async function sendMessageWithRetry(ctx, message, retryCount = 3, delay = 1000) {
-  while (retryCount > 0) {
-    try {
-      await ctx.reply(message);
-      return;
-    } catch (error) {
-      if (
-        error.description === "Too Many Requests: retry after 5" &&
-        retryCount > 0
-      ) {
-        await wait(delay);
-        delay *= 2; // Exponential backoff
-        retryCount--;
-      } else {
-        throw error;
-      }
-    }
-  }
-}
+// Help command handler
+bot.command("help", (ctx) => {
+  const helpMessage = `
+  Available commands:
+  /start - Start the bot
+  /help - Show help message
+  /ping - Check bot's response time
+  `;
+  ctx.reply(helpMessage);
+});
 
-// Helper function to wait for a specific time
-function wait(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
+// Ping command handler
+bot.command("ping", (ctx) => {
+  const start = Date.now();
+  ctx.reply("Pinging...");
+  const end = Date.now();
+  const responseTime = end - start;
+  ctx.reply(`Pong! Response time: ${responseTime}ms`);
+});
+
+// Error handler
+bot.catch((err) => {
+  console.error("Error:", err);
+});
+
+// Start the bot
+bot.start();
 
 // Start the bot using long polling
 bot.start({
